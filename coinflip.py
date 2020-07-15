eaf907d59a92edbf7902079cc6e0fcd9f639e273
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-15 07:20:14 +0100

    add: brainstorming

---
+++ coinflip.py
@@ -0,0 +1,6 @@
+'''Brainstorming
+    T -> number of testcases
+        integer G -> at first line of each T
+            integer I N Q -> at the first line of each G
+    N -> number of coins -> all Head or Tail at the beginning
+'''
