62658a1bb4f0ac84d2dbd1fb0421ae4ffa93936d
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-15 08:37:48 +0100

    add: complete brainstorming

--- coinflip.py
+++ coinflip.py
@@ -1,6 +1,9 @@
 '''Brainstorming
-    T -> number of testcases
-        integer G -> at first line of each T
-            integer I N Q -> at the first line of each G
-    N -> number of coins -> all Head or Tail at the beginning
+    T -> number of testcases, range(1,10)
+        integer G -> at first line of each T, range(1, 2000)
+            I N Q -> at the first line of each G
+    integer N -> number of coins -> all Head or Tail at the beginning, range(1, 109)
+    integer I(initial state of coins, 1(guess number of heads at the end) or 2(guess number of tails at the end)) 
+    integer N(number of coins and rounds), range(1, 109)
+    integer Q(1(guess number of heads at the end) or 2(guess number of tails at the end))
 '''
