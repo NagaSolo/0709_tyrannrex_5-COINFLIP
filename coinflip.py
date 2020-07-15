1a243a1a5ab631b6c5f5f63cfe6ae991a87a9c0d
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-15 09:14:28 +0100

    add: basic scaffold for codes

--- coinflip.py
+++ coinflip.py
@@ -2,8 +2,41 @@
     T -> number of testcases, range(1,10)
         integer G -> at first line of each T, range(1, 2000)
             I N Q -> at the first line of each G
-    integer N -> number of coins -> all Head or Tail at the beginning, range(1, 109)
     integer I(initial state of coins, 1(guess number of heads at the end) or 2(guess number of tails at the end)) 
     integer N(number of coins and rounds), range(1, 109)
     integer Q(1(guess number of heads at the end) or 2(guess number of tails at the end))
 '''
+def q_is(Q):
+    ''' number of head or tail, arg head or tail'''
+    if (int(Q) == 1):
+        print(1)
+    elif (int(Q) == 2):
+        print(2)
+    else:
+        exit()
+
+def flip_coin():
+    ''' flip the coin for each rounds'''
+    print('WIP')
+
+T = int(input())
+if T>1 and T<=10:
+    for test in range(T):
+        G = int(input())
+        if G>1 and G<=2000:
+            for game in range(G):
+                I, N, Q = input().strip().split()
+                if (int(I) == 1):
+                    pass
+                elif (int(I) == 2):
+                    pass
+                else:
+                    exit()
+                for rounds in range(int(N)):
+                    flip_coin()
+                if (int(Q) == 1):
+                    print(q_is(Q))
+                elif (int(Q) == 2):
+                    print(q_is(Q))
+                else:
+                    exit()
